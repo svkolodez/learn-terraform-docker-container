bf9399730c2491509fa4da23b16ba41f829b0490
Sergei Kolodeznykh <svkolodez@gmail.com>
2021-11-28 11:31:21 +0300

    Load balancer - 2

--- prod.tf
+++ prod.tf
@@ -24,13 +24,6 @@ resource "aws_default_subnet" "default_az2" {
   }
 }
 
-resource "aws_default_subnet" "default_az3" {
-  availability_zone = "eu-central-1c"
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
 resource "aws_security_group" "prod_web" {
   name = "prod_web"
   description = "Allow standard http and https ports inbound and everything outbound"
@@ -59,35 +52,9 @@ resource "aws_security_group" "prod_web" {
   }
 }
 
-resource "aws_instance" "prod_web" {
-  count = 2
-
-  ami = "ami-01b9dc827d1e2d177"
-  instance_type = "t2.nano"
-
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id
-  ]
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
-resource "aws_eip_association" "prod_web" {
-  instance_id = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
-
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Terraform" : "true"
-  }
-}
-
 resource "aws_elb" "prod_web" {
   name = "prod-web"
-  instances = aws_instance.prod_web.*.id
-  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id, aws_default_subnet.default_az3.id]
+  subnets = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   security_groups = [aws_security_group.prod_web.id]
 
   listener {
@@ -101,3 +68,36 @@ resource "aws_elb" "prod_web" {
   }
 }
 
+resource "aws_launch_template" "prod_web" {
+  name_prefix = "prod-web"
+  image_id = "ami-01b9dc827d1e2d177"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.prod_web.id]
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+#  availability_zones = ["eu-central-1a","eu-central-1b"]
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+
+  desired_capacity = 2
+  max_size         = 2
+  min_size         = 1
+
+  launch_template {
+    id = aws_launch_template.prod_web.id
+    version = "$Latest"
+  }
+  tag {
+    key = "Terraform"
+    value = "true"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
+}
